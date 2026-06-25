b320dcb9028bfd64ce5e68c0cf1b4437f8a6a8ca
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 21:04:29 +0500

    loading image function

--- Phase 1 in Open cv/loading-image.py
+++ Phase 1 in Open cv/loading-image.py
@@ -1,6 +1,6 @@
 import cv2
 
-image=cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg")
+image=cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg") #here imread is function which will load ur picture and chk it
 if image is None:
      print("Error image is not found :")
 else:
