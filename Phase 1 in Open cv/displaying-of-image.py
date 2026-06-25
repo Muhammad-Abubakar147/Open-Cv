6917adda6c1df87d786f60e0dbef71f9867920d3
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 23:08:59 +0500

    Displaying of image in Open Cv

--- Phase 1 in Open cv/displaying-of-image.py
+++ Phase 1 in Open cv/displaying-of-image.py
@@ -1,6 +1,6 @@
-#Imshow
-#waitkey
-#distroyallwindows
+#Imshow => it will show image on computer screen 
+#waitkey => if we are not using it after imshow it will disappear from screen instantly 
+#distroyallwindows => will close all open images on computer screen
 #There are methods of dispalying methods
 
 import cv2
@@ -8,7 +8,7 @@ import cv2
 image=cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg") 
 if image is not None:
      cv2.imshow("Image displaying",image) #open the window
-     cv2.waitKey(0) #wait for a key
+     cv2.waitKey(0) #wait for a key (here 0 means whwnever any key is press close the image)
      cv2.destroyAllWindows()#close the window
 else:
     print("Image not found")
