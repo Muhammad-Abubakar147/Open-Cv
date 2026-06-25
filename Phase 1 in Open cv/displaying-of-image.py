7a9effd67028973f1976df63eaa619bed6f3fbfa
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 21:43:16 +0500

    Displyaing of image in Open Cv

--- Phase 1 in Open cv/displaying-of-image.py
+++ Phase 1 in Open cv/displaying-of-image.py
@@ -2,3 +2,13 @@
 #waitkey
 #distroyallwindows
 #There are methods of dispalying methods
+
+import cv2
+
+image=cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg") 
+if image is not None:
+     cv2.imshow("Image displaying",image) #open the window
+     cv2.waitKey(0) #wait for a key
+     cv2.destroyAllWindows()#close the window
+else:
+    print("Image is found")
