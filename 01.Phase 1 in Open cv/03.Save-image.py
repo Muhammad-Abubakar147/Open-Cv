cc5c6d6c1264fa1f8f3df1a7dbc90defb643014e
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 15:34:34 +0500

    03.Save image Method in Open-cv

--- 01.Phase 1 in Open cv/03.Save-image.py
+++ 01.Phase 1 in Open cv/03.Save-image.py
@@ -1,4 +1,6 @@
+#Here we will discuss how to save an image in open cv
 import cv2 #importing open cv
+
 image=cv2.imread("Phase 1 in Open cv\opencv-.jpg") #reading image 
 success=cv2.imwrite("Output.jpeg",image) #saving image 
 print(success)
