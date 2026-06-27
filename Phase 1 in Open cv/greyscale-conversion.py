23593ad7ad80f6bbb221547627adb99f95907a07
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 14:27:48 +0500

    Grayscale conversion method

--- Phase 1 in Open cv/greyscale-conversion.py
+++ Phase 1 in Open cv/greyscale-conversion.py
@@ -1,1 +1,2 @@
 #Grayscle conversion method is a method in which we can change color type of image .
+#Here we will use method for changing color of image which is => "cvtColor" it will convert color of image .
