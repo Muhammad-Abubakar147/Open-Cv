6f7c8989f8992801a4f9dffb4ea6b7cfb3f825ba
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 20:34:36 +0500

    Resizing an image in open cv

--- 02.Phase 2 in open cv/01.Resizing&Reshaping.py
+++ 02.Phase 2 in open cv/01.Resizing&Reshaping.py
@@ -8,3 +8,10 @@ image= cv2.imread("photos\cat image 2.jpg")
 if image is None:
     print("Image not loaded")
     
+else:
+    print("Image loaded")
+    resized=cv2.resize(image,(360,350))
+    cv2.imshow(" Orignal Image is showing",image)
+    cv2.imshow("Resized image is showing",resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
