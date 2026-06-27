bba88cfbc17970bf33e590902ab49add2073b1b6
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 14:16:13 +0500

    Image dimension method

--- Phase 1 in Open cv/image-dimension-method.py
+++ Phase 1 in Open cv/image-dimension-method.py
@@ -3,3 +3,8 @@
 
 import cv2
 image =cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg")
+if image is not None:
+    h,w,c=image.shape
+    print(f"Image loaded:/nHieght: {h}/nWidth: {w}/nChannels: {c}")
+else:
+    print("Image not loaded successfully")
