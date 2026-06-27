cfa5c6ffa258cf0d833b1bbb68a66e60e2961742
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 20:26:32 +0500

    resing an image

--- 02.Phase 2 in open cv/01.Resizing&Reshaping.py
+++ 02.Phase 2 in open cv/01.Resizing&Reshaping.py
@@ -1,1 +1,3 @@
-#Here we will disscus how to resize and reshape an image 
+#Here we will disscus how to resize and reshape an image in open cv .
+
+#syntax for this method => resize=cv2.resize(sorce,width,hieght,)
