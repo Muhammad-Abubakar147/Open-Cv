ea9f6762aef2c31124130956b15cb2e404947b6a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 20:28:59 +0500

    resizing image

--- 02.Phase 2 in open cv/01.Resizing&Reshaping.py
+++ 02.Phase 2 in open cv/01.Resizing&Reshaping.py
@@ -1,3 +1,10 @@
 #Here we will disscus how to resize and reshape an image in open cv .
 
 #syntax for this method => resize=cv2.resize(sorce,width,hieght,)
+
+import cv2
+image= cv2.imread("photos\cat image 2.jpg")
+
+if image is None:
+    print("Image not loaded")
+    
