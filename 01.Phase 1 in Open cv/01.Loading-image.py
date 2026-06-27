e13b7145a0ae5b2cecaa87d6922d5b4a8d1f5085
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-27 15:32:03 +0500

    01.Loading Image Methos in Open-cv

--- 01.Phase 1 in Open cv/01.Loading-image.py
+++ 01.Phase 1 in Open cv/01.Loading-image.py
@@ -1,5 +1,7 @@
 import cv2 #importing open cv
 
+#Here we will discuus how to load an image in open cv
+
 image=cv2.imread("Open-Cv\Phase 1 in Open cv\opencv-.jpg") #here imread is function which will load ur picture and chk it
 if image is None: #condition statement
      print("Error image is not found :")
