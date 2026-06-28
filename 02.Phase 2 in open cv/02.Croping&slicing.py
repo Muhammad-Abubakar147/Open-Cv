565b1c0f833e3d37b43d70c8a5dce9270e21d375
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-28 14:54:48 +0500

    croping method

--- 02.Phase 2 in open cv/02.Croping&slicing.py
+++ 02.Phase 2 in open cv/02.Croping&slicing.py
@@ -6,7 +6,7 @@ import cv2 as cv  #importing cv
 image =cv.imread("photos\cat iamge.jpg") #image read
 
 if image is not None:
-    croped=image[100:500,100:500]
+    croped=image[100:500,100:500] #crope method syntax
     cv.imshow("ORignal",image)
     cv.imshow("Cropped",croped)
     cv.waitKey(0)
