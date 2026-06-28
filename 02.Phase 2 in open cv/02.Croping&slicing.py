21c44af47bf916a1280b29eb3bfa2550179df0fe
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-28 14:43:00 +0500

    croped image

--- 02.Phase 2 in open cv/02.Croping&slicing.py
+++ 02.Phase 2 in open cv/02.Croping&slicing.py
@@ -1,2 +1,7 @@
 #Here we will discuss how to crop and slice an image in open-cv
 #Crope image method use for croping some area from the picture in open cv
+
+import cv2 as cv 
+
+image =cv.imread("photos\mother cat image.jpg")
+croped=image[100:200,150:50]
