f7fb0ea88bf82b63177d9a4c20ae762834eab4df
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-28 15:18:36 +0500

    counter clock wise

--- 02.Phase 2 in open cv/Image-Rotation & Fliping.py
+++ 02.Phase 2 in open cv/Image-Rotation & Fliping.py
@@ -2,4 +2,4 @@
 #Syntax for rotation => "rotated = cv.rotate(image, cv.ROTATE_90_CLOCKWISE)"
 # Available Rotation Options :
 # cv.ROTATE_90_CLOCKWISE => for rotating clockwise
-#cv.ROTATE_90_COUNTERCLOCKWISE
+#cv.ROTATE_90_COUNTERCLOCKWISE => for counter clockwis erotation of image 
