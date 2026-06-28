82b6baefb545a2889a9057612309de16f848a94d
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-28 14:53:43 +0500

    croping image

--- 02.Phase 2 in open cv/02.Croping&slicing.py
+++ 02.Phase 2 in open cv/02.Croping&slicing.py
@@ -1,7 +1,7 @@
 #Here we will discuss how to crop and slice an image in open-cv
 #Crope image method use for croping some area from the picture in open cv
 
-import cv2 as cv 
+import cv2 as cv  #importing cv 
 
 image =cv.imread("photos\cat iamge.jpg")
 if image is not None:
