2002cc9332b38d7cdf481af9b4215477903099b9
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-29 18:56:05 +0500

    Image rotation

--- 02.Phase 2 in open cv/Image-Rotation & Fliping.py
+++ 02.Phase 2 in open cv/Image-Rotation & Fliping.py
@@ -12,7 +12,7 @@ image =cv.imread("photos\cat image 2.jpg")
 (h,w)=image.shape[:2]
 center=(w//2,h//2)
 M=cv.getRotationMatrix2D(center,120,1.0)
-rotated=cv.warpAffine(image,M(w,h))
+rotated=cv.warpAffine(image, M, (w, h))
 cv.imshow("Orignal Image",image)
 cv.imshow("Rotated Image",rotated)
 cv.waitKey(0)
