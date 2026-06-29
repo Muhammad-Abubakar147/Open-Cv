83bac9bea83322a5028e43419a7d5bf5c4915059
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-29 20:13:51 +0500

    04.Fliping of image in Open cv

--- 02.Phase 2 in open cv/04.Fliping-of-image.py
+++ 02.Phase 2 in open cv/04.Fliping-of-image.py
@@ -3,3 +3,23 @@
 #syntax for flip an image in open cv => cv.flip(image,1=horizontally)
                                     #=> cv.flip(image,0=vertcally)
                                     #=> cv.flip(image,-1=both)
+                        
+                        
+import cv2 as cv
+
+image =cv.imread("C:\Data\Open Cv\Open-Cv\photos\cat iamge.jpg")
+
+if image is None:
+    print("Could not load image")
+    exit()
+else:
+    fliped_horizontal=cv.flip(image,1)
+    fliped_vertically=cv.flip(image,0)
+    fliped_both=cv.flip(image,-1)
+    
+    cv.imshow("Flipped Horizonatally",fliped_horizontal)
+    cv.imshow("Fliped Vertically",fliped_vertically)
+    cv.imshow("Flipped_both",fliped_both)
+    
+    cv.waitKey(0)
+    cv.destroyAllWindows()
