22154eddfda150c5598c66d99cb91c3667ded177
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-29 20:04:37 +0500

    fliping of image in open cv

--- 02.Phase 2 in open cv/04.Fliping-of-image.py
+++ 02.Phase 2 in open cv/04.Fliping-of-image.py
@@ -1,4 +1,5 @@
 #Here we will disscuss about fliping of image in open cv 
 
 #syntax for flip an image in open cv => cv.flip(image,1=horizontally)
-                                    #=> cv.flip(image 0=vertcally)
+                                    #=> cv.flip(image,0=vertcally)
+                                    #=> cv.flip(image,-1=both)
