23aa6107f6bdf25347b4b5be0f2ede65ff1c3c5a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-30 21:09:49 +0500

    Draw line function in open cv

---
+++ 03.Phase 3 in Open cv/Line-Draw.py
@@ -0,0 +1,16 @@
+#Here we will discus about how to draw a line on image 
+
+import cv2 
+
+image =cv2.imread("photos\Cat image 3.jpg")
+
+pt1=(20,20)
+pt2=(200,200)
+color=(250,0,0)
+thicknes=(4)
+
+cv2.line(image,pt1,pt2,color,thicknes)
+
+cv2.imshow("Image Showing",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
