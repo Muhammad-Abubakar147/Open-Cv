af33732f47afb02de2a85d168bf21ef33b8904b6
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 16:03:42 +0500

    comment out

--- 03.Phase 3 in Open cv/Text-image.py
+++ 03.Phase 3 in Open cv/Text-image.py
@@ -2,9 +2,11 @@
 #Syntax for texting on a image 
 
 #cv.putText(image,"text",org,font,font scale,color,thickness)
-
+#importing cv 
 import cv2 as cv
+#Loading image 
 image=cv.imread("photos\mother cat image.jpg")
+#checking image shape
 print(image.shape)
 cv.putText(image,"This is mother image",(10,150),
            cv.FONT_HERSHEY_PLAIN,1.2,(255,0,0),1)
