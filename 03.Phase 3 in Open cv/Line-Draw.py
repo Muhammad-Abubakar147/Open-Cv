aaf5e5dcd8f402f0cf657022b700e5eed49cca83
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 14:04:00 +0500

    Line draw function

--- 03.Phase 3 in Open cv/Line-Draw.py
+++ 03.Phase 3 in Open cv/Line-Draw.py
@@ -6,9 +6,11 @@ image =cv2.imread("photos\Cat image 3.jpg") #Loading image
 
 pt1=(20,20)
 pt2=(200,200)
+#define color of line 
 color=(250,0,0)
+#How much thickness of line 
 thicknes=(4)
-
+#Line draw syntax 
 cv2.line(image,pt1,pt2,color,thicknes)
 
 cv2.imshow("Image Showing",image)
