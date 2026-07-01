c9d0d75fffd3b2d9d8741bdf0f6c6dcb62e3799a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 16:02:43 +0500

    Syntax

--- 03.Phase 3 in Open cv/Text-image.py
+++ 03.Phase 3 in Open cv/Text-image.py
@@ -4,5 +4,10 @@
 #cv.putText(image,"text",org,font,font scale,color,thickness)
 
 import cv2 as cv
-image=cv.imshow("photos\mother cat image.jpg")
-cv.putText(image,"This is mother image",(50,200),cv.FONT_HERSHEY_PLAIN,1.2,(255,0,0),2)
+image=cv.imread("photos\mother cat image.jpg")
+print(image.shape)
+cv.putText(image,"This is mother image",(10,150),
+           cv.FONT_HERSHEY_PLAIN,1.2,(255,0,0),1)
+cv.imshow("texting image show",image)
+cv.waitKey(0)
+cv.destroyAllWindows()
