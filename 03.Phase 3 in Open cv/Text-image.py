3fca6c0c19058cb8262d5f111869b77691270360
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 16:04:57 +0500

    Text image function

--- 03.Phase 3 in Open cv/Text-image.py
+++ 03.Phase 3 in Open cv/Text-image.py
@@ -8,8 +8,12 @@ import cv2 as cv
 image=cv.imread("photos\mother cat image.jpg")
 #checking image shape
 print(image.shape)
+#syntax of image texting
 cv.putText(image,"This is mother image",(10,150),
            cv.FONT_HERSHEY_PLAIN,1.2,(255,0,0),1)
+#image show
 cv.imshow("texting image show",image)
+#waitkey
 cv.waitKey(0)
+#close all windows
 cv.destroyAllWindows()
