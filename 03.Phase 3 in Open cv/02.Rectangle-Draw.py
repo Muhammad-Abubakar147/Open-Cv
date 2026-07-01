00358e57e8d77dafcbba21943294954faca5b492
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 14:24:35 +0500

    Rectangle

--- 03.Phase 3 in Open cv/02.Rectangle-Draw.py
+++ 03.Phase 3 in Open cv/02.Rectangle-Draw.py
@@ -1,3 +1,11 @@
 # Syntax of Draw a Rectangle in open cv 
 #cv.rectangle(image,pt,pt,color,thickness)
 
+import cv2 
+
+image =cv2.imread("photos\cat image 2.jpg")
+
+pt1=(50,50)
+pt2=(200,200)
+color=(250,0,0)
+thickness=(5)
