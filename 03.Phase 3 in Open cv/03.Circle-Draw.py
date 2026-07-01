d67af4ec1d7190963873a530c107af946a2685d2
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-01 14:45:34 +0500

    03.Circle draw function

--- 03.Phase 3 in Open cv/03.Circle-Draw.py
+++ 03.Phase 3 in Open cv/03.Circle-Draw.py
@@ -1,12 +1,12 @@
 #Syntax for draw cirle in open cv 
 #cv.circle(image,center,radius,color,thickness)
-
+#importing of cv 
 import cv2 as cv
-
+#Loading an image 
 image=cv.imread("photos\cat image 2.jpg")
-
+#draw circle function
 cv.circle(image,(210,150),50,(0,0,250),3)
-
+#image show syntax
 cv.imshow("Circle image",image)
 cv.waitKey(0)
 cv.destroyAllWindows()
