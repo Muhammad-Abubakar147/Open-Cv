eeebafd7a963dd8e945c9429805af8cec0d1ea2a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-03 22:22:06 +0500

    01.Guassian blur image method

--- 05.Phase 5 in Open cv/01.Guassian-blur.py
+++ 05.Phase 5 in Open cv/01.Guassian-blur.py
@@ -2,3 +2,15 @@
 
 #syntax for this method 
 #blur image=cv2.GuassianBlur(image,(kernal_size_x,kernal_size_y),sigma)
+
+import cv2
+#Loading of image 
+image =cv2.imread(r"photos\Nature.jpg") 
+#syntax for guassian blur image 
+blury =cv2.GaussianBlur(image,(7,7),6)
+#image show orignal
+cv2.imshow("Orignal image",image)
+#image blur show 
+cv2.imshow("Blury image",blury)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
