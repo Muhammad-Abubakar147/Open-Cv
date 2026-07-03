22862bfc756adc9ef62dddd139828045272b039d
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-03 22:54:00 +0500

    added

--- 05.Phase 5 in Open cv/03.Sharpening.py
+++ 05.Phase 5 in Open cv/03.Sharpening.py
@@ -1,2 +1,3 @@
 #Here we will disscuss about sharpening of image in open cv . It will make sharper ur image .It highlight ur image edges .
 #syntax for this method : cv2.filter2D(src,ddept,kernal)
+
