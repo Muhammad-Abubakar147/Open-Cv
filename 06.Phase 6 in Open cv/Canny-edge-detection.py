6122ed4b9ede5141d9dc4515cb775f24c0b9eb5b
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-04 14:36:32 +0500

    Canny edge dtection

---
+++ 06.Phase 6 in Open cv/Canny-edge-detection.py
@@ -0,0 +1,3 @@
+#Purpose of this method to detect edges (boundries of image ). => For separation of objects => Features extraction 
+
+#Syntax for this method :  => edge = cv2.Canny(image,thrsehold 1,threshold 2)
