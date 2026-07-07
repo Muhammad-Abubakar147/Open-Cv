a92f3fda1715c4114eb71cc5d5cb4c19856dc807
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-07 15:36:28 +0500

    02.Thresherhold image

--- 06.Phase 6 in Open cv/02.Threshold-image.py
+++ 06.Phase 6 in Open cv/02.Threshold-image.py
@@ -5,8 +5,10 @@ import cv2
 image =cv2.imread(r"photos\camerman.jpg",cv2.IMREAD_GRAYSCALE)
 #synta for tresherhold image
 ret,thershold_img =cv2.threshold(image,120,255,cv2.THRESH_BINARY)
-
+#show orignal image 
 cv2.imshow("orignal image",image)
+#show thresherhold image
 cv2.imshow("treshhold image",thershold_img)
+#close all windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
