bcb8f50c5f02af80340435693f1068e46b84e4e1
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-07 15:35:26 +0500

    thresherhold image

--- 06.Phase 6 in Open cv/02.Threshold-image.py
+++ 06.Phase 6 in Open cv/02.Threshold-image.py
@@ -1,9 +1,9 @@
 #syntax of threshlod image : threshold_image=cv2.threshold(image,thresh-value,max_value,method)
-
+#importing cv
 import cv2 
-
+#loading image 
 image =cv2.imread(r"photos\camerman.jpg",cv2.IMREAD_GRAYSCALE)
-
+#synta for tresherhold image
 ret,thershold_img =cv2.threshold(image,120,255,cv2.THRESH_BINARY)
 
 cv2.imshow("orignal image",image)
