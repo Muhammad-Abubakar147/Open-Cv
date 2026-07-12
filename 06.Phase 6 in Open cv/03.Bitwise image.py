5b9acad9b63afedba187266c3bf3705b2b3345a2
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-12 15:39:54 +0500

    03.Bitwise operations

--- 06.Phase 6 in Open cv/03.Bitwise image.py
+++ 06.Phase 6 in Open cv/03.Bitwise image.py
@@ -3,4 +3,30 @@
 #There are three types of bitwise operation in open cv 
 #cv2.bitwise_or(image1,image2)
 #cv2.bitwise_and(image1,image2)
-#cv2.bitwise_not(image1,image2)
+#cv2.bitwise_not(image1,)
+
+
+import cv2
+import numpy as np
+
+img1=np.zeros((300,300),dtype="uint8")
+img2=np.zeros((300,300),dtype="uint8")
+
+cv2.circle(img1,(150,150),100,255,-1)
+cv2.rectangle(img2,(100,100),(250,250),255,-1)
+
+bitwise_and=cv2.bitwise_and(img1,img2)
+bitwise_or=cv2.bitwise_or(img1,img2)
+bitwise_not=cv2.bitwise_not(img1)
+
+cv2.imshow("Circle image",img1)
+cv2.imshow("rectangle image",img2)
+
+cv2.imshow("Bitwise and Image ",bitwise_and)
+
+cv2.imshow("Bitwise or Image",bitwise_or)
+
+cv2.imshow("Bittwise not Image",bitwise_not)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
